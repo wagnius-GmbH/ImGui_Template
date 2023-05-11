3ed84c858b5f5d35d2fec4a36f45d66c023692d9
Florian Wagner <slvwagner@gmail.com>
2023-05-11 13:06:58 +0200

    Projektdateien hinzufügen.

---
+++ ImGuiTemplate/ImGuiTemplate.cpp
@@ -0,0 +1,12 @@
+﻿// ImGuiTemplate.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "ImGuiTemplate.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
