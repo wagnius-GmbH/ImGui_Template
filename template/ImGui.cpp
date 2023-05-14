d6153f67bedf212e4315af43532dcd2fb9e5fe8f
Florian Wagner <slvwagner@gmail.com>
2023-05-14 15:57:11 +0200

    Non used variable

--- template/ImGui.cpp
+++ template/ImGui.cpp
@@ -5,7 +5,6 @@ public:
 	void Init(GLFWwindow* window, const char* glsl_version) {
 		IMGUI_CHECKVERSION();
 		ImGui::CreateContext();
-		ImGuiIO& io = ImGui::GetIO();
 		// Setup Platform/Renderer bindings
 		ImGui_ImplGlfw_InitForOpenGL(window, true);
 		ImGui_ImplOpenGL3_Init(glsl_version);
