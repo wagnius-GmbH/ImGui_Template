46841eed5e11d2cbe6fcf16fd9574adbd618dde2
Florian Wagner <slvwagner@gmail.com>
2023-05-14 15:52:02 +0200

    First running ImGui

---
+++ template/ImGui.cpp
@@ -0,0 +1,50 @@
+#include "header.h"
+
+class UseImGui {
+public:
+	void Init(GLFWwindow* window, const char* glsl_version) {
+		IMGUI_CHECKVERSION();
+		ImGui::CreateContext();
+		ImGuiIO& io = ImGui::GetIO();
+		// Setup Platform/Renderer bindings
+		ImGui_ImplGlfw_InitForOpenGL(window, true);
+		ImGui_ImplOpenGL3_Init(glsl_version);
+		ImGui::StyleColorsDark();
+	}
+	void NewFrame() {
+		// feed inputs to dear imgui, start new frame
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+	};
+	virtual void Update() {
+		// CustomImGui Hello, world! 
+		ImGui::Begin("CustomImGui Hello, world!");              // Create a window called "Hello, world!" and append into it.
+		ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+		ImGui::End();
+	}
+	void Render() {
+		// Render dear imgui into screen
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+	}
+	void Shutdown() {
+		// Cleanup
+		ImGui_ImplOpenGL3_Shutdown();
+		ImGui_ImplGlfw_Shutdown();
+		ImGui::DestroyContext();
+	}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
--- template/ImGuiTemplate.cpp
+++ template/ImGuiTemplate.cpp
@@ -1,77 +1,38 @@
-﻿// ImGuiTemplate.cpp: Definiert den Einstiegspunkt für die Anwendung.
-//
+﻿// Dear ImGui: standalone example application for GLFW + OpenGL 3, using programmable pipeline
+// (GLFW is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan/Metal graphics context creation, etc.)
+// If you are new to Dear ImGui, read documentation from the docs/ folder + read the top of imgui.cpp.
+// Read online: https://github.com/ocornut/imgui/tree/master/docs
 
-#include "ImGuiTemplate.h"
-#include <implot_internal.h>
-using namespace std;
+#include "ImGui.cpp"
 
-string version = "0.1.0";
-
-void UseImGui::Init(GLFWwindow* window, const char* glsl_version) {
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO();
-	// Setup Platform/Renderer bindings
-	ImGui_ImplGlfw_InitForOpenGL(window, true);
-	ImGui_ImplOpenGL3_Init(glsl_version);
-	ImGui::StyleColorsDark();
-}
-
-void UseImGui::NewFrame() {
-	// feed inputs to dear imgui, start new frame
-	ImGui_ImplOpenGL3_NewFrame();
-	ImGui_ImplGlfw_NewFrame();
-	ImGui::NewFrame();
-}
-
-
-void UseImGui::Update() {
-	ImGui::Begin("My First Tool");
-	// Generate samples and plot them
-	float samples[100];
-	for (int n = 0; n < 100; n++)
-		samples[n] = sinf(n * 0.2f + ImGui::GetTime() * 1.5f);
-	ImGui::PlotLines("Samples", samples, 100);
-
-	ImGui::End();
-}
-
-void UseImGui::Render() {
-	// Render dear imgui into screen
-	ImGui::Render();
-	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-}
-
-void UseImGui::Shutdown() {
-	// Cleanup
-	ImGui_ImplOpenGL3_Shutdown();
-	ImGui_ImplGlfw_Shutdown();
-	ImGui::DestroyContext();
-}
-
-
-
-
+/// <summary>
+/// Testing ImGui 
+/// show Pictures and web cam video in ImGui
+/// </summary>
+/// <returns></returns>
 int main()
 {
-	cout << "Programm Start" << "\nV" << version << endl;
+	// Setup window
+	if (!glfwInit())
+		return 1;
 
 	// GL 3.0 + GLSL 130
 	const char* glsl_version = "#version 130";
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
 	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
 	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
 
 	// Create window with graphics context
 	GLFWwindow* window = glfwCreateWindow(1280, 720, "Dear ImGui - Example", NULL, NULL);
 	if (window == NULL)
-		cout << "Coud not create GLFW object" << "\nThis is a major errror\nProgramm Exit" << endl;
 		return 1;
 	glfwMakeContextCurrent(window);
 	glfwSwapInterval(1); // Enable vsync
+
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))  // tie window context to glad's opengl funcs
 		throw("Unable to context to OpenGL");
+
 	int screen_width, screen_height;
 	glfwGetFramebufferSize(window, &screen_width, &screen_height);
 	glViewport(0, 0, screen_width, screen_height);
@@ -79,7 +40,7 @@ int main()
 	// Create GUI object
 	UseImGui myimgui;
 	myimgui.Init(window, glsl_version);
-	// Load image to display just once
+	
 
 	// ImGui update
 	while (!glfwWindowShouldClose(window)) {
@@ -94,3 +55,4 @@ int main()
 
 	return 0;
 }
+
